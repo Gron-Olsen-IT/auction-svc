bbf6cc05a6fb775a317cdcca5b4ee57295b720d3
Mads Lajer <eaamalr@students.eaaa.dk>
2023-12-01 11:38:40 +0100

    Interfaces for Service and Repo started

--- AuctionAPI/Models/Auction.cs
+++ AuctionAPI/Models/Auction.cs
@@ -15,3 +15,22 @@ public record Auction{
     public string EmployeeId { get; set; }
     public int Status { get; set; }
 }
+
+public record AuctionDTO{
+
+    public AuctionDTO(TimeSpan timeSpan, int minPrice, string productId, string employeeId, int status)
+    {
+        TimeSpan = timeSpan;
+        MinPrice = minPrice;
+        ProductId = productId;
+        EmployeeId = employeeId;
+        Status = status;
+    }  
+
+    public TimeSpan TimeSpan { get; set; }
+    public int MinPrice { get; set; }
+    public string ProductId { get; set; }
+    public string EmployeeId { get; set; }
+    public int Status { get; set; }
+
+}
