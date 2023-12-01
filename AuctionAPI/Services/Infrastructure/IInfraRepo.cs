e7ae45a4b935a167c4b55d68824d484744a902af
Mads Lajer <eaamalr@students.eaaa.dk>
2023-12-01 14:33:58 +0100

    Maybe work

--- AuctionAPI/Services/Infrastructure/IInfraRepo.cs
+++ AuctionAPI/Services/Infrastructure/IInfraRepo.cs
@@ -1,7 +1,15 @@
 using System.Net;
+using AuctionAPI.Models;
 
 namespace AuctionAPI.Services;
 
 public interface IInfraRepo {
+
+    public Task<List<Tuple<string, string>>> GetMaxBid(List<string> auctionIds);
+
+    /* For Redis DB Cached keyvalues - Future
+    public Task<List<Auction>> GetProduct(string auctionId);
+    */
+
     
 }
